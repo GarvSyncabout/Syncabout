5a156872e30fc09476e534fab599df6c1818f821
GarvSyncabout <sbs.gs2024@gmail.com>
2024-11-20 16:35:48 +0530

    mejor changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        "https://cdn.jsdelivr.net/npm/preline@2.5.1/dist/preline.min.js",
+      ],
+    },
+  },
+});
