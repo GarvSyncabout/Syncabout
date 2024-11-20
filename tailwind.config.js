dac31c43977e26b61794bf6d13fe92dd6cca3a53
GarvSyncabout <sbs.gs2024@gmail.com>
2024-11-20 10:14:15 +0530

    Mejor Chnages

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Pontano: ['"Pontano Sans"', ...defaultTheme.fontFamily.sans],
+        Rajdhani: ['"Rajdhani"', ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
